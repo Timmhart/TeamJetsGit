4c19a26b431ed0f7d0c04d176d0c5f889878e31b
Timmhart <tim.hart@hva.nl>
2023-05-12 15:54:21 +0200

    Redberries fix

--- Assets/Scripts/Redberry.cs
+++ Assets/Scripts/Redberry.cs
@@ -5,26 +5,37 @@ using UnityEngine;
 public class Redberry : MonoBehaviour
 {
     public GameObject redberryUI;
+    public bool berry = true;
 
-    private void Start()
+    private void Awake()
     {
+        Debug.Log("Redberries not active");
         redberryUI.SetActive(false);
 
     }
-//   private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if(other.gameObject.name == "Grace" && other.gameObject.name == "Harry")
-//         {
-        
-//         }
-//     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Grace" && other.gameObject.name == "Harry")
+        {
+            berry = true;
+        }
+    }
     void OnTriggerStay2D(Collider2D other)
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (berry == false)
+        {
+            berry = true; //we dont want to load the scene multiple times
+            // Destroy(gameObject);
+        }   
+    }
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
         {
             redberryUI.SetActive(true);
             Destroy(gameObject);
-
-        }   
+        }
     }
 }
