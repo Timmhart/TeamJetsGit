75a13e6847fda4274a93adfb7585f42f1704ba14
sharon <svanopzeeland@kpnmail.nl>
2023-05-12 14:21:17 +0200

    Farmhuisje DONE
    alleen dialogue farmer

---
+++ Assets/Scripts/LoadSceneUitHuis.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneUitHuis : MonoBehaviour
+{
+
+  public bool loadHuis = false;
+
+  private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Grace" && other.gameObject.name == "Harry")
+        {
+            loadHuis = false;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (loadHuis == true)
+        {
+            loadHuis = false; //we dont want to load the scene multiple times
+            SceneManager.LoadScene("Farm");
+        }   
+    }
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            loadHuis = true;
+        }
+    }
+}
