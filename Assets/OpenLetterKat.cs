43c37a7c3e93eeabde507a89134adb2466eac6de
Timmhart <tim.hart@hva.nl>
2023-05-12 17:39:03 +0200

    Shooting fixed
    Shooting fixed
     bug fixes
    letter added

---
+++ Assets/OpenLetterKat.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLetterKat : MonoBehaviour
+{
+    public GameObject letterKat;
+
+    private void Awake()
+    {
+        Debug.Log("JOOOOEEEEEOOEOEOOEOEOEOEOOEOEOEOE");
+        letterKat.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Harry"))
+        {
+            letterKat.SetActive(true);
+        }
+    }
+
+     private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Harry"))
+        {
+            letterKat.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/RemoveBush.cs
+++ Assets/Scripts/RemoveBush.cs
@@ -1,40 +1,53 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class RemoveBush : MonoBehaviour
+// {
+// //   private bool bush = true;
+//   public GameObject poppy;
+
+//   void Start()
+//   {
+//       poppy.SetActive(false);
+//   }
+
+//   private void OnTriggerEnter2D(Collider2D other)
+//     {
+//         if(other.gameObject.name == "Grace" && other.gameObject.name == "Harry")
+//         {
+//             // bush = true;
+//         }
+//     }
+//     void OnTriggerStay2D(Collider2D other)
+//     {
+//          if (Input.GetKeyDown(KeyCode.E)) 
+//         {
+//             Debug.Log("bush is gone");
+//             // bush = false;
+//             Destroy(gameObject);
+//             poppy.SetActive(true);
+//         }
+//         // if (bush == false)
+//         // {
+//         //     bush = true; //we dont want to load the scene multiple times
+//         //     Destroy(gameObject);
+//         // }   
+//     }
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RemoveBush : MonoBehaviour
 {
-  private bool bush = true;
-  public GameObject poppy;
-
-  void Start()
-  {
-      poppy.SetActive(false);
-  }
-
-  private void OnTriggerEnter2D(Collider2D other)
+  private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.name == "Grace" && other.gameObject.name == "Harry")
+        if(other.gameObject.CompareTag("Bullet"))
         {
-            bush = true;
-        }
-    }
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (bush == false)
-        {
-            bush = true; //we dont want to load the scene multiple times
+            Debug.Log("HALLLLLLOOOOOOOOOOOOO");
             Destroy(gameObject);
-        }   
-    }
- 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E)) 
-        {
-            Debug.Log("bush is gone");
-            bush = false;
-            poppy.SetActive(true);
         }
-    }
+    } 
 }
